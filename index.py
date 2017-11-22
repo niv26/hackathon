21651e544ffb47edf6e67b4bda6957b861b406cb
ariel7342 <ariel7342@gmail.com>
2017-11-22 17:20:14 +0200

    1 - init

---
+++ index.py
@@ -0,0 +1,23 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import urllib, simplejson,unicodedata
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+url = "http://iiif.nli.org.il/collections/root.json"
+
+rootPage = simplejson.loads(urllib.urlopen(url).read())
+collections = rootPage['collections']
+
+for collection in collections:
+    urlLibrary = collection['@id']
+    # print urlLibrary
+    librariesPage = simplejson.loads((urllib.urlopen(urlLibrary).read()))
+    urlImages = librariesPage['members']
+    for urlImg in urlImages:
+        print urlImg
+        imgStr = (urllib.urlopen(urlImg['@id']).read()).decode('utf-8').encode('utf-8')
+        imgPage = simplejson.loads(imgStr)
+        print imgPage
